9f18d709f3c7e81b10c21c299f4ba5dfa05796dc
Lambda <lambda@Abrahams-MacBook-Pro.local>
2019-10-03 09:31:00 +0100

    print room class

--- src/room.py
+++ src/room.py
@@ -11,7 +11,11 @@ class Room:
         self.directions = directions
     # methods
     def __str__(self):
-        return f"In room {self.name}.\n{self.description} it is located in the {self.directions}"
+        return f"room {self.name}.\n{self.description} is located in the {self.directions}"
 
     def __repr__(self):
         return f"{self.name}, {self.description}, {self.directions}"
+
+location = Room('Eko', 'the cafeteria', 'n')
+
+print(location)
