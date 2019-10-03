47394dbfcb97d049781ea3021b8324228b245f59
Lambda <lambda@Abrahams-MacBook-Pro.local>
2019-10-03 05:34:34 +0100

    setup classes

--- src/player.py
+++ src/player.py
@@ -1,2 +1,7 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+class Player:
+    def __init__(self, name, age, location):
+        self.name = name
+        self.age = age
+        self.location = location
