fe015cccc7f7e4a1f3e043947a549b56feb38411
Lambda <lambda@Abrahams-MacBook-Pro.local>
2019-10-03 09:09:02 +0100

    create room and player class

--- src/player.py
+++ src/player.py
@@ -1,7 +1,40 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+# create a player item and some items to pick up
+# then create a room and add the player to the room
+
+# import rooms for composition fields
+from room import Room
+
+
 class Player:
-    def __init__(self, name, age, location):
+    def __init__(self, name, age, current_room):
         self.name = name
         self.age = age
-        self.location = location
+        self.current_room = current_room
+
+        # methods
+
+        # getters
+    def get_name(self):
+        return self.name
+
+        # setters
+    def set_name(self, name):
+        self.name = name
+
+    def __str__(self):
+        output = ""
+        output += self.name + "\n" + "age" + self.age
+        i = 1
+        for c in self.current_room:
+            output += "  " + str(i) + ". " + c.name + "\n"
+            i += 1
+
+        # add an exit message
+        output += "  " + str(i) + ". Exit"
+        return output
+
+
+s = Player('Player 1', 'Eko', [Room('Big Apple', 'NYC', 'n'), Room(
+    'Gold Coast', 'Ghana', 's'), Room('Safari', 'SA', 'e'), Room('Naija', 'Nigeria', 'w')])
--- src/room.py
+++ src/room.py
@@ -1,9 +1,17 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
-
+# Create a Room class with a name and a contains[] and a method add_item
+# Create an Item class with a name and a contains[] that can print contents and pick up item
+from player import Player
 class Room:
     # constructor
-    def __init__(self, name, description):
+    def __init__(self, name, description, directions):
         self.name = name
         self.description = description
+        self.directions = directions
     # methods
+    def __str__(self):
+        return f"In room {self.name}.\n{self.description} it is located in the {self.directions}"
+
+    def __repr__(self):
+        return f"{self.name}, {self.description}, {self.directions}"
